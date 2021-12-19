efab471499f55c8483e6e190d3dd97d671a6d55d
ST3607 <>
2021-12-19 22:09:50 +0000

    Files

--- src/exercise9/EllipseDemo.java
+++ src/exercise9/EllipseDemo.java
@@ -10,13 +10,13 @@ public class EllipseDemo {
       Ellipse ellipse4 = new Ellipse();
       ellipse4.initialize();
 
-      System.out.println("Ellipse 1:" + ellipse1);
-      System.out.println("Ellipse 2:" + ellipse2);
-      System.out.println("Ellipse 3:" + ellipse3);
-      System.out.println("Ellipse 4:" + ellipse4);
+      System.out.println("Ellipse 1: " + ellipse1);
+      System.out.println("Ellipse 2: " + ellipse2);
+      System.out.println("Ellipse 3: " + ellipse3);
+      System.out.println("Ellipse 4: " + ellipse4);
 
-      System.out.println("Ellipse 2 equals Ellipse 1:" + ellipse2.equals(ellipse1));
-      System.out.println("Ellipse 2 equals Ellipse 3:" + ellipse2.equals(ellipse3));
+      System.out.println("Ellipse 2 equals Ellipse 1: " + ellipse2.equals(ellipse1));
+      System.out.println("Ellipse 2 equals Ellipse 3: " + ellipse2.equals(ellipse3));
 
     }
 }
